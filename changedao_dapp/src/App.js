6d931a4de85d5a3468fcf8db43cddb802313dfae
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2022-11-20 00:18:30 -0600

    Added thirdweb connect wallet

--- changedao_dapp/src/App.js
+++ changedao_dapp/src/App.js
@@ -2,8 +2,31 @@ import { useAddress, useNetwork, ConnectWallet, useContract, useNFTBalance, Web3
 import { useState, useEffect, useMemo } from 'react';
 import { AddressZero } from "@ethersproject/constants";
 import { ChainId } from '@thirdweb-dev/sdk';
+import styled from "styled-components";
+import './App.css';
 
 function App() {
+
+  const address = useAddress();
+  console.log("Welcome Address: ", address);
+
+
+  if(!address) {
+    return (
+      <div className="landing">
+        <img src={`https://file.rendit.io/n/UXG4I2PJnr0vTyuJX98U.png`} />
+        <h1>Welcome to Change Dao</h1>
+        <div className="btn-hero">
+          <ConnectWallet 
+          colorMode="dark"
+          accentColor="#06b6d4"
+        
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div><h1>Hello world!</h1></div>
   );
