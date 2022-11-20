44445607d0a5ec6237695c33dc8c9f9c9d0516a4
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2022-11-19 23:59:07 -0600

    create react app baseline

---
+++ changedao_dapp/src/App.js
@@ -0,0 +1,12 @@
+import { useAddress, useNetwork, ConnectWallet, useContract, useNFTBalance, Web3Button } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
+import { AddressZero } from "@ethersproject/constants";
+import { ChainId } from '@thirdweb-dev/sdk';
+
+function App() {
+  return (
+    <div><h1>Hello world!</h1></div>
+  );
+}
+
+export default App;
